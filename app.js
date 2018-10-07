4c0378674cc7617f4d1ac4c5b0e405273e0064ca
WANDEE SREPENRAK <59050300@kmitl.ac.th>
2018-10-07 12:14:00 +0700

    add process.env.PORT

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ const api = require('./route/api')
 const www = require('./route/www')
 const bodyParser = require('body-parser')
 const app = express()
-const port = 3000
+const port = (process.env.PORT || '3000');
 
 app.use(bodyParser.urlencoded({ extended: true }));
 
